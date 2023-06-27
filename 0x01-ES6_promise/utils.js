7918a96df640cbf4ed94d0f631b14e0c54caab94
sambilokonga <abebawbegna@gmail.com>
2023-06-27 08:56:24 -0700

    task 3

--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -1,26 +1,15 @@
-// utils.js
-
-// Simulated asynchronous function to upload a photo
 export function uploadPhoto() {
-  return new Promise((resolve, reject) => {
-    // Simulating a delay with setTimeout
-    setTimeout(() => {
-      const photoData = { body: 'profile-1' };
-      resolve(photoData);
-    }, 2000); // Simulating a 2-second delay
+  return Promise.resolve({
+    status: 200,
+    body: 'photo-profile-1',
   });
 }
 
-// Simulated asynchronous function to create a user
+
 export function createUser() {
-  return new Promise((resolve, reject) => {
-    // Simulating a delay with setTimeout
-    setTimeout(() => {
-      const userData = {
-        firstName: 'Guillaume',
-        lastName: 'Salva'
-      };
-      resolve(userData);
-    }, 1500); // Simulating a 1.5-second delay
+  return Promise.resolve({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
   });
 }
+

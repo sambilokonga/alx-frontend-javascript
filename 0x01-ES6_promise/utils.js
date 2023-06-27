d3bb5afa9b199c1dd5491300118e710ed5a43ef6
sambilokonga <abebawbegna@gmail.com>
2023-06-27 08:52:54 -0700

    utils

--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -1,15 +1,26 @@
+// utils.js
+
+// Simulated asynchronous function to upload a photo
 export function uploadPhoto() {
-  return Promise.resolve({
-    status: 200,
-    body: 'photo-profile-1',
+  return new Promise((resolve, reject) => {
+    // Simulating a delay with setTimeout
+    setTimeout(() => {
+      const photoData = { body: 'profile-1' };
+      resolve(photoData);
+    }, 2000); // Simulating a 2-second delay
   });
 }
 
-
+// Simulated asynchronous function to create a user
 export function createUser() {
-  return Promise.resolve({
-    firstName: 'Guillaume',
-    lastName: 'Salva',
+  return new Promise((resolve, reject) => {
+    // Simulating a delay with setTimeout
+    setTimeout(() => {
+      const userData = {
+        firstName: 'Guillaume',
+        lastName: 'Salva'
+      };
+      resolve(userData);
+    }, 1500); // Simulating a 1.5-second delay
   });
 }
-

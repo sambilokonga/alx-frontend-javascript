d9e15ef1928ad9a24baa8cb43c6a0d4ebe5fc2fc
sambilokonga <abebawbegna@gmail.com>
2023-06-28 09:04:59 -0700

    last update

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,3 @@
-/*
- * The function should return a Promise rejecting with an Error and the string $fileName cannot be processed
-*/
-
 export default function uploadPhoto(filename) {
   return Promise.reject(Error(`${filename} cannot be processed`));
 }

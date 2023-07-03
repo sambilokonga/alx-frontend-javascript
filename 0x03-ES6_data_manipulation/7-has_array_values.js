e90bfa494943e937fafdb2dd41502e15d5f7d994
sambilokonga <abebawbegna@gmail.com>
2023-07-03 08:54:06 -0700

    updated

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
-/*
- * a function named hasValuesFromArray that returns a boolean if all the elements in the array exist within the set
- * It accepts two arguments: a set (Set) and an array (Array).
- */
-
 export default function hasValuesFromArray(set, array) {
   return array.every((value) => set.has(value));
 }

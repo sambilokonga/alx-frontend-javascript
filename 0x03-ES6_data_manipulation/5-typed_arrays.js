ff6e9cc2c9e1d731a6d051ebabbb8463ab485a8b
sambilokonga <abebawbegna@gmail.com>
2023-07-03 08:50:22 -0700

    updated

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,3 @@
-/*
- * a function named createInt8TypedArray that returns a new ArrayBuffer with an Int8 value at a specific position
- */
-
 export default function createInt8TypedArray(length, position, value) {
   const arr = new ArrayBuffer(length);
   const val = new DataView(arr);

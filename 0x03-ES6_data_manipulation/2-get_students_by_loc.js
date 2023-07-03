8e51287eaed40b74f715d48f577a1ca4b9318796
sambilokonga <abebawbegna@gmail.com>
2023-07-03 08:46:06 -0700

    updated

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,3 @@
-/*
- * a function getStudentsByLocation that returns an array of objects who are located in a specific city
- */
-
 export default function getStudentsByLocation(students, city) {
   return students.filter((obj) => obj.location === city);
 }
